c97fe0f7f631712f916a56951083c0b0e12fac0e
vtbac1999 <83001581+vtbac1999@users.noreply.github.com>
2023-02-15 17:18:12 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,20 +23,20 @@ function Footer() {
                     <div className="footer__widget">
                         <h6>Useful Links</h6>
                         <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">About Our Shop</a></li>
-                            <li><a href="#">Secure Shopping</a></li>
-                            <li><a href="#">Delivery infomation</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Our Sitemap</a></li>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">About Our Shop</a></li>
+                            <li><a href="">Secure Shopping</a></li>
+                            <li><a href="">Delivery infomation</a></li>
+                            <li><a href="">Privacy Policy</a></li>
+                            <li><a href="">Our Sitemap</a></li>
                         </ul>
                         <ul>
-                            <li><a href="#">Who We Are</a></li>
-                            <li><a href="#">Our Services</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Innovation</a></li>
-                            <li><a href="#">Testimonials</a></li>
+                            <li><a href="">Who We Are</a></li>
+                            <li><a href="">Our Services</a></li>
+                            <li><a href="">Projects</a></li>
+                            <li><a href="">Contact</a></li>
+                            <li><a href="">Innovation</a></li>
+                            <li><a href="">Testimonials</a></li>
                         </ul>
                     </div>
                 </div>
@@ -44,15 +44,15 @@ function Footer() {
                     <div className="footer__widget">
                         <h6>Join Our Newsletter Now</h6>
                         <p>Get E-mail updates about our latest shop and special offers.</p>
-                        <form action="#">
+                        <form action="">
                             <input type="text" placeholder="Enter your mail"/>
                             <button type="submit" className="site-btn">Subscribe</button>
                         </form>
                         <div className="footer__widget__social">
-                            <a href="#"><i className="fa fa-facebook"></i></a>
-                            <a href="#"><i className="fa fa-instagram"></i></a>
-                            <a href="#"><i className="fa fa-twitter"></i></a>
-                            <a href="#"><i className="fa fa-pinterest"></i></a>
+                            <a href=""><i className="fa fa-facebook"></i></a>
+                            <a href=""><i className="fa fa-instagram"></i></a>
+                            <a href=""><i className="fa fa-twitter"></i></a>
+                            <a href=""><i className="fa fa-pinterest"></i></a>
                         </div>
                     </div>
                 </div>
